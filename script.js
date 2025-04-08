ac4141a6f926a7abac4b6e1b48bfa3f29ff4fa89
Mustapha <abc29.mustapha@gmail.com>
2025-04-08 14:43:35 +0200

    exo

---
+++ script.js
@@ -0,0 +1,11 @@
+const ticket = {
+    NomFilm: "Charlot",
+    Prix: 15,
+    NumSalle: 1,
+};
+
+let Nom = "Mustapha";
+
+let affichage = "Bonjour " + Nom + ", votre film " + ticket.NomFilm + " est en salle numéro " + ticket.NumSalle;
+
+console.log(affichage)
